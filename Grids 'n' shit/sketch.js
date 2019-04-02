a596d7758c116eafe9d654fcf4e5c99b62cb44bb
irekimpton <47368861+irekimpton@users.noreply.github.com>
2019-04-02 15:21:36 -0600

    grids and AI

--- Grids 'n' shit/sketch.js
+++ Grids 'n' shit/sketch.js
@@ -5,12 +5,17 @@
 // Extra for Experts:
 // - describe what you did to take this project "above and beyond"
 
-let gridSize = 10;
+let gridSize = 50;
 let grid;
 let cellSize;
 
 function setup() {
-  createCanvas(windowWidth, windowHeight);
+  if (windowHeight > windowWidth){
+    createCanvas(windowWidth, windowWidth);
+  }
+  else{
+    createCanvas(windowHeight, windowHeight);
+  }
   cellSize = height/gridSize;
   grid = createRandom2dArray(gridSize, gridSize);
 }
@@ -54,4 +59,56 @@ function displayGrid(){
       rect(x*cellSize, y*cellSize, cellSize, cellSize);
     }
   }
+}
+
+function mousePressed(){
+  let xCoord = floor(mouseX/cellSize);
+  let yCoord = floor(mouseY/cellSize);
+  console.log(xCoord, yCoord);
+  if (grid[yCoord][xCoord] === 1){
+    grid[yCoord][xCoord] = 0
+  }
+  else{
+    grid[yCoord][xCoord] = 1
+  }
+}
+
+function keyPressed(){
+  if (key === " "){
+    updateGame()
+  }
+  if (key === "w"){
+    grid = create2dArray(gridSize, gridSize);
+  }
+}
+
+function updateGame(){
+  let newGrid = create2dArray(gridSize, gridSize);
+  for (y = 0; y < gridSize; y++){
+    for (x = 0; x < gridSize; x++){
+      let friends = 0;
+      for (y2 = -1; y2 < 2; y2++){
+        for (x2 = -1; x2 < 2; x2++){
+          if (y+y2 >= 0 && x+x2 >= 0 && y+y2 < gridSize && x+x2 < gridSize){
+            friends += grid[y+y2][x+x2];
+          }
+        }
+      }
+      friends -= grid[y][x];
+      if (grid[y][x] === 1){
+        if (friends === 2 || friends === 3){
+          newGrid[y][x] = 1;
+        }
+        else{
+          newGrid[y][x] = 0;
+        }
+      }
+      else if (grid[y][x] === 0){
+        if (friends === 3){
+            newGrid[y][x] = 1;
+        }
+      }
+    }
+  }
+  grid = newGrid;
 }
